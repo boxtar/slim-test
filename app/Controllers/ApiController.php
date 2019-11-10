2fc8ec4b5cab2c0769601bc6ae2f383eebe843e8
Johnpaul McMahon <jmcmah15@live.com>
2019-11-10 21:35:39 +0000

    Refactored to use Guzzle Async Requests

---
+++ app/Controllers/ApiController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Property;
+use GuzzleHttp\Client;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Flash\Messages;
+
+use function GuzzleHttp\Promise\settle;
+
+class ApiController
+{
+    // Guzzle Client
+    protected $client;
+
+    // API endpoint
+    protected $uri = "http://trialapi.craig.mtcdevserver.com/api/properties";
+
+    // API key
+    protected $key = '3NLTTNlXsi6rBWl7nYGluOdkl2htFHug';
+
+    // Number of property resources per page (100 is max)
+    protected $pageSize = 100;
+
+    // Constructore
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Ping API to get 'total' property. This is the total number of
+     * properties available from the API. Use the total number to
+     * figure out how many concurrent requests are needed based on the number
+     * of properties required per request.
+     * 
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     */
+    public function index(RequestInterface $request, ResponseInterface $response)
+    {
+        // This will hold the transformed API data for storing in DB
+        $transformedData = $this->transformApiData($this->getDataFromApi());
+
+        // Save or Update Property in Database.
+        // This needs to be optimised.
+        foreach ($transformedData as $apiProperty) {
+            // Is this API property already in our own Datastore?
+            if ($property = Property::where('api_id', $apiProperty['uuid'])->get()->first()) {
+                // If yes, just update it.
+                $property->update($apiProperty);
+            } else {
+                // Else persist it in our Datastore.
+                Property::create($apiProperty);
+            }
+        }
+
+        // Flash message to session
+        (new Messages())->addMessage('notifications', 'Properties updated.');
+
+        // Redirect to property index
+        return $response
+            ->withHeader(
+                'Location',
+                $request->getAttribute('routeParser')->urlFor('properties.index')
+            );
+    }
+
+    protected function getDataFromApi()
+    {
+        // This array will hold all the promises for the async requests.
+        $promises = [];
+        for ($i = 1; $i <= $this->getNumberOfRequestsRequired(); $i++) {
+            $promises[] = $this->client->getAsync($this->uri, [
+                'query' => [
+                    'api_key' => $this->key,
+                    'page[number]' => $i,
+                    'page[size]' => $this->pageSize
+                ]
+            ]);
+        }
+
+        return settle($promises)->wait();
+    }
+
+    /**
+     * Ping API quickly for the total property count.
+     * This will allow us to calculate the no. of
+     * requests we need to make.
+     */
+    protected function getNumberOfRequestsRequired()
+    {
+        $options = [
+            'query' => [
+                'api_key' => $this->key,
+                'page[number]' => 1,
+                'page[size]' => 1
+            ]
+        ];
+        // Total no of properties available from API
+        $totalProperties = json_decode(
+            $this->client->get($this->uri, $options)->getBody()->getContents(),
+            true
+        )['total'];
+        // Ok, so this is the number of concurrent requests we need to fire off.
+        return (int) ceil($totalProperties / $this->pageSize);
+    }
+
+    /**
+     * Perform a few necessary transformations of the API data.
+     */
+    protected function transformApiData($data)
+    {
+        $transformedData = [];
+        // Do some transforming to each property and add to our collection
+        foreach ($data as $result) {
+            $data = json_decode($result['value']->getBody()->getContents(), true)['data'];
+            foreach ($data as $property) {
+                $transformedData[] = array_merge(
+                    (array) $property,
+                    [
+                        'property_type' => $property['property_type']['title'],
+                        'api_id' => $property['uuid']
+                    ]
+                );
+            }
+        }
+        return $transformedData;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
+use App\Controllers\ApiController;
 use Slim\App;
-use App\Controllers\HomeController;
-use App\Controllers\UsersController;
-use App\Controllers\PropertiesController;
 use App\Models\Property;
+use Slim\Flash\Messages;
+use App\Controllers\HomeController;
+use App\Controllers\PropertiesController;
+use GuzzleHttp\Promise\Promise;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Flash\Messages;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+use function GuzzleHttp\Promise\settle;
 
 return function (App $app) {
 
@@ -21,6 +24,10 @@ return function (App $app) {
         // Index
         $group->get('', PropertiesController::class . ':index')
             ->setName('properties.index');
+
+        // Refresh from API
+        $group->get('/refresh', ApiController::class . ':index')
+            ->setName('properties.refresh');
 
         // Create
         $group->get('/create', PropertiesController::class . ':create')
@@ -46,95 +53,4 @@ return function (App $app) {
         $group->delete('/{id}', PropertiesController::class . ':delete')
             ->setName('properties.delete');
     });
-
-    // Contact the API
-    // TODO: Implement error handling
-    // TODO: Refactor
-    // TODO: Look into asyc Guzzle requests
-    $app->get('/get-data', function (Request $request, Response $response) {
-
-        // Uncomment below when testing so api isn't hammered:
-
-        // // Flash message to session
-        // (new Messages())->addMessage('notifications', 'Properties updated.');
-
-        // return $response
-        //     ->withHeader(
-        //         'Location',
-        //         $request->getAttribute('routeParser')->urlFor('properties.index')
-        //     );
-
-        // API key
-        $apiKey = '3NLTTNlXsi6rBWl7nYGluOdkl2htFHug';
-
-        // Page being requested
-        $currentPage = 1;
-
-        // Number of property resources per page (100 is max)
-        $pageSize = 100;
-
-        // Next URL for data from API
-        $nextUrl = "http://trialapi.craig.mtcdevserver.com/api/properties";
-
-        // After the below while loop runs this array will hold all the property data from API
-        $apiData = [];
-
-        // Setup Guzzle client
-        $client = new GuzzleHttp\Client();
-
-        // $nextUrl is updated with API response so this loop will
-        // continue until API returns a falsy value for the next URL.
-        while ($nextUrl) {
-            // Using getContents takes ALL data from the PSR7 Stream Response object.
-            $data = $client->get($nextUrl, [
-                'query' => [
-                    'api_key' => $apiKey,
-                    'page[number]' => $currentPage,
-                    'page[size]' => $pageSize
-                ]
-            ])
-                ->getBody()
-                ->getContents();
-
-            $data = json_decode($data);
-
-            // Do some transforming to each property and add to our collection
-            foreach ($data->data as $property) {
-                $apiData[] = array_merge(
-                    (array) $property,
-                    [
-                        'property_type' => $property->property_type->title,
-                        'api_id' => $property->uuid
-                    ]
-                );
-            }
-
-            // Time to get the next page from the API...
-            $currentPage += 1;
-
-            // ... But only if there are any pages left.
-            $nextUrl = $data->next_page_url;
-        }
-
-        // Ok - Time for some more looping.
-        foreach ($apiData as $apiProperty) {
-            // Is this API property already in our own Datastore?
-            if ($property = Property::where('api_id', $apiProperty['uuid'])->get()->first()) {
-                // If yes, just update it.
-                $property->update($apiProperty);
-            } else {
-                // Else persist it in our Datastore.
-                Property::create($apiProperty);
-            }
-        }
-
-        // Flash message to session
-        (new Messages())->addMessage('notifications', 'Properties updated.');
-
-        return $response
-            ->withHeader(
-                'Location',
-                $request->getAttribute('routeParser')->urlFor('properties.index')
-            );
-    })->setName('properties.refresh-from-api');
 };
